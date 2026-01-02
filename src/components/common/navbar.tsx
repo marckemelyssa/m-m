72005aba7f12256cfd007d246c53c267282e1e3b
Caio Noronha <caio.cnoronha@gmail.com>
2026-01-02 20:35:11 -0300

    fixing navbar

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -14,6 +14,7 @@ export default function NavBar() {
   const navLinks = [
     { label: "The Code", href: "/home" },
     { label: "About Us", href: "/about" },
+    { label: "Contact", href: "/contact" }
   ];
 
   React.useEffect(() => {
